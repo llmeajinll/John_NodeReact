aef4d5f574d87814091e7de60e67a026398eff92
unknown <khj000217@naver.com>
2022-06-30 03:43:54 +0900

    비밀정보호보

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongose = require('mongoose')
-mongoose.connect('mongodb+srv://KimHyejin:bubbler0217@cluster0.b6dba.mongodb.net/?retryWrites=true&w=majority',{})
+mongoose.connect(config.monogoURI,{})
 .then(()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 이야 된다')
+  res.send('Hello World! 자야되는디')
+})
+
+app.post('/register', (req,res)=>{
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+
+  const user = new User(req.body)
+  user.save((err, doc)=>{
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
